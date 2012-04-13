f6fd728cfc23eeb724f394a2807bd95816815793
egdinger <egdinger@gmail.com>
2012-04-13 16:55:14 -0700

    Changed serial speed to reflect new limits when using serial over xbee

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pubsub
 
 if __name__=='__main__':
 	logging.basicConfig(level=logging.DEBUG)
-	lfc = llfc.Llfc('/dev/ttyUSB0', 115200, pubsub.PubSubServer('', 8090))
+	lfc = llfc.Llfc('/dev/ttyUSB0', 57600, pubsub.PubSubServer('', 8090))
 	cgw = controlgw.ControlGw('localhost', 8091, lfc)
 	evloop.EventDispatcher().loop_forever()
 
