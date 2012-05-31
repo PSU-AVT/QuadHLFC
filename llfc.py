0213fa41e203621a7f61e845687fbc5fe89ce44c
Gregory Haynes <greg@greghaynes.net>
2012-05-31 15:24:12 -0700

    Skeleton message handler framework in llfc.py

--- llfc.py
+++ llfc.py
@@ -27,25 +27,26 @@ class Llfc(afprotowatcher.SerialAfprotoWatcher):
 
 	def __init__(self, path='/dev/ttyUSB0', baudrate=115200, pubsub_server=None):
 		afprotowatcher.SerialAfprotoWatcher.__init__(self, path, baudrate)
-		self.msg_handlers = {
-			2: self.handle_debug_msg,
-			3: self.handle_error_msg,
-			4: self.handle_gyro_state,
-			5: self.handle_accelero_state,
-			6: self.handle_intertial_state,
-			7: self.handle_motors_state,
-			8: self.handle_setpoint_state }
-		self.pubsub_server = pubsub_server
-
-	def publish(self, tag, msg):
-		try:
-			self.pubsub_server.publish('' + tag + ': ' + msg)
-		except AttributeError:
-			pass
+		str_unpack = lambda x: str(struct.unpack('%ds' % len(x), x))
+		state_unpack = lambda x: struct.unpack('%6f' % len(x))
+		recv_cmds = {
+			2: ('debug_msg', str_unpack),
+			3: ('error_msg', str_unpack),
+			4: ('gyro_state', state_unpack),
+			5: ('accelerometer_state', state_unpack),
+			6: ('inertial_state', state_unpack),
+			7: ('motors_state', lambda x: struct.unpack('4f', x)),
+			8: ('setpoint_state', state_unpack),
+			}
+		self.msg_handlers = {}
 
 	def handle_msg(self, msg):
 		try:
-			self.msg_handlers[ord(msg[0])](msg)
+			cmd_id = ord(msg[0])
+			recv_cmd = self.recv_cmds[cmd_id]
+			name = recv_cmd[0]
+			unpacker = recv_cmd[1]
+			self.msg_handlers[name](*unpacker(msg[1:]))
 		except KeyError:
 			pass
 
@@ -57,36 +58,6 @@ class Llfc(afprotowatcher.SerialAfprotoWatcher):
 		logging.debug('LLFC Debug: %s' % msg)
 
 		self.publish('LlfcDebug', msg)
-
-	def handle_error_msg(self, msg):
-		# trash the msg id byte
-		msg = msg[1:]
-
-		# log error message
-		logging.error('LLFC Error: %s' % msg)
-
-		self.publish('LlfcError', msg)
-
-	def handle_gyro_state(self, msg):
-		logging.debug('LLFC Gyro State:\tRoll: %f\tPitch: %f\tYaw: %f' % struct.unpack('fff', msg[1:]))
-		self.publish('LlfcStateGyro', msg[1:])
-
-	def handle_accelero_state(self, msg):
-		logging.debug('LLFC Accelero State:\tX: %f\tY: %f\tZ: %f' % struct.unpack('fff', msg[1:]))
-		self.publish('LlfcStateAccelero', msg[1:])
-
-	def handle_intertial_state(self, msg):
-		logging.debug('LLFC Inertial State:\tRoll: %f\tPitch: %f\tYaw: %f' % struct.unpack('fff', msg[1:13]))
-		self.publish('LlfcStateInertial', msg[1:])
-
-	def handle_motors_state(self, msg):
-		logging.debug('Llfc Motors State: \t1: %f\t2: %f\t3: %f\t4: %f' % struct.unpack('ffff', msg[1:]))
-		self.publish('LlfcStateMotors', msg[1:])
-
-	def handle_setpoint_state(self, msg):
-		logging.debug('Llfc Setpoint: \tRoll: %f\tPitch: %f\tYaw: %f' % struct.unpack('fff', msg[1:13]))
-		print len(msg)
-		self.publish('LlfcStateSetpoint', msg[1:])
 
 	def send_command(self, cmd_id, data):
 		self.send_msg(chr(cmd_id) + data)
