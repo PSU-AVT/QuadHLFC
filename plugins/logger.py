d8850e64de1a1b331d5b571a3c5643ddf8a67cbc
Gregory Haynes <greg@greghaynes.net>
2012-06-10 02:21:20 -0700

    Properly init logging handler

--- plugins/logger.py
+++ plugins/logger.py
@@ -5,7 +5,7 @@ import llfc
 
 class Logger(plugin.Plugin):
 	def __init__(self):
-		llfc.Llfc().add_cmd_handler(self.debug)
+		llfc.Llfc().add_cmd_handler('debug_msg', self.debug)
 
 	def debug(self, msg):
 		logging.debug('Quadcopter: ' %s)
