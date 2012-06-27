12151d4ee479914db9685769103532e7c1e4215f
Gregory Haynes <greg@greghaynes.net>
2012-06-27 11:12:55 -0700

    Logger handles errors

--- plugins/logger.py
+++ plugins/logger.py
@@ -6,6 +6,10 @@ from llfc import llfc
 class Logger(plugin.Plugin):
 	def __init__(self):
 		llfc.add_cmd_handler('debug', self.debug)
+		llfc.add_cmd_handler('error', self.error)
 
 	def debug(self, msg):
 		logging.debug('Quadcopter: ' %s)
+
+	def error(self, msg):
+		logging.error('Quadcopter: ' %s)
