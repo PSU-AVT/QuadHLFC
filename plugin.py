89b7e06274db043c83af9944d450b550c7d19087
Gregory Haynes <greg@greghaynes.net>
2012-06-27 11:13:02 -0700

    Added docs to plugin.py

--- plugin.py
+++ plugin.py
@@ -1,5 +1,15 @@
 class Plugin(object):
+	'''Base class for all plugins
+	   to create a plugin, subclass this and place in the plugins
+	   directory.'''
+
+	enabled = True # Set to false in subclasses to disable that plugin
+
 	def activate(self):
+		'''Called after plugin is loaded but before added to event
+		   loop'''
 		pass
+
 	def deactivate(self):
+		'''Called before plugin is unloaded'''
 		pass
