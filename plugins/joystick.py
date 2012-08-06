aa007fce55d5cd5be9200d48d64175cb2531ce15
Jenner Hanni <jeh.wicker@gmail.com>
2012-08-05 21:45:40 -0700

    Making stuff up

--- plugins/joystick.py
+++ plugins/joystick.py
@@ -1,17 +1,28 @@
-import logging
+# import logging
 import sys # what is sys?
 
-import plugin
-from llfc import llfc
+# import plugin
+# from llfc import llfc
 
 # find out which port the joystick uses
 #  heavily depends on http://hackshark.com/?p=147#axzz22jTSqLWt
 
-pipe = open('/dev/js0','r')
+pipe = open('/dev/hidraw0','r')
 
+action = []
+spacing = 0
 while 1:
         for character in pipe.read(1):
-                sys.stdout.write(repr(character))
-                sys.stdout.flush()
+                action += [character]
+                if len(action) == 8:
+                        for byte in action:
+                                sys.stdout.write('%02X ' % ord(byte))
+                        spacing += 1
+                        if spacing == 2:
+                                sys.stdout.write('\n')
+                                spacing = 0
+                        sys.stdout.write('\n')
+                        sys.stdout.flush()
+                        action = []
 
 
