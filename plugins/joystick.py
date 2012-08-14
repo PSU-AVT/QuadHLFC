2bc210fe495a7e232834518cca642cec7a01bed7
Gregory Haynes <greg@greghaynes.net>
2012-08-13 21:09:40 -0700

    Added joystick skeleton

---
+++ plugins/joystick.py
@@ -0,0 +1,20 @@
+import llfc
+import plugin
+import evloop
+
+class JoystickWatcher(evloop.FdWatcher):
+	def __init__(self, dev_path):
+		super(JoystickWatcher, self).__init__()
+		f = open(dev_path)
+		self.fd = fd
+		self.setup_fd(f, 0)
+		self.set_readable(True)
+
+	def handle_read(self, fd):
+		msg = self.fd.read(8)
+		print 'Got joystick event'
+
+class JoystickPlugin(plugin.Plugin):
+	def __init__(self):
+		self.joystick_watcher = JoystickWatcher('/dev/js0')
+
