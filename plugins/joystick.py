cc637f4361a2c76d7a95303d73ac3a223acd7a29
Gregory Haynes <greg@greghaynes.net>
2012-08-13 21:22:09 -0700

    Decode joystick events

--- plugins/joystick.py
+++ plugins/joystick.py
@@ -1,6 +1,18 @@
 import llfc
 import plugin
 import evloop
+import struct
+
+class JoystickEvent(object):
+	@staticmethod
+	def init_from_linux_raw(msg):
+		return JoystickEvent(*struct.unpack("IhBB", msg))
+
+	def __init__(self, time, value, event_type, number):
+		self.time = time
+		self.value = value
+		self.event_type = event_type
+		self.number = number
 
 class JoystickWatcher(evloop.FdWatcher):
 	def __init__(self, dev_path):
@@ -12,7 +24,7 @@ class JoystickWatcher(evloop.FdWatcher):
 
 	def handle_read(self, fd):
 		msg = self.fd.read(8)
-		print 'Got joystick event'
+		event = JoystickEvent.init_from_linux_raw(msg)
 
 class JoystickPlugin(plugin.Plugin):
 	enabled = False
